25d98b815b094591de02c24ffbcba1e37bfa6f17
azizguindo <abazizguido@gmail.com>
2020-03-05 15:10:37 +0100

    Raster

--- Utils.hpp
+++ Utils.hpp
@@ -2,30 +2,35 @@
 #define UTILS_H
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-void triangle(Vec2f p1,Vec2f p2,Vec2f p3,TGAImage & image,TGAColor color);
-void line(Vec2f p0, Vec2f p1 , TGAImage &image, TGAColor color) ;
+void triangle(Vec2f t1,Vec2f t2,Vec2f t3,TGAImage & image,TGAColor color);
 //algo Bresenham
-void line2(Vec2f p0, Vec2f p1 , TGAImage &image, TGAColor color);
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 
 
-void triangle(Vec2f p1,Vec2f p2,Vec2f p3,TGAImage & image,TGAColor color){
-    if (p1.y>p2.y) std::swap(p1, p2); 
-    if (p1.y>p3.y) std::swap(p1, p3); 
-    if (p2.y>p3.y) std::swap(p2, p3); 
-    line(p1,p2,image,color);
-    line(p2,p3,image,color);
-    line(p1,p3,image,color);
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    if (t0.y==t1.y && t0.y==t2.y) return; // i dont care about degenerate triangles
+    if (t0.y>t1.y) std::swap(t0, t1);
+    if (t0.y>t2.y) std::swap(t0, t2);
+    if (t1.y>t2.y) std::swap(t1, t2);
+    int total_height = t2.y-t0.y;
+    for (int i=0; i<total_height; i++) {
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y;
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+        float alpha = (float)i/total_height;
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+        Vec2i A =               t0 + (t2-t0)*alpha;
+        Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
+        if (A.x>B.x) std::swap(A, B);
+        for (int j=A.x; j<=B.x; j++) {
+            image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y
+        }
+    }
 }
-void line(Vec2f p0, Vec2f p1 , TGAImage &image, TGAColor color) {
-    double a=(p0.y-p1.y)/(p0.x-p1.x);
-    double b=p0.y-a*p0.x;
-		for (int x=p0.x; x<=p1.x; x++) {
-            float y=a*x+b;
-			image.set(x, y, color);
-	 } 
-}
+
+
+
 //algo Bresenham
-void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	bool steep = false;
 	if (std::abs(x0-x1)<std::abs(y0-y1)) {
 		std::swap(x0, y0);
@@ -45,7 +50,7 @@ void line2(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
             image.set(x, y, color);
         }
     }
-	
+
 }
 
 
--- main.cpp
+++ main.cpp
@@ -13,21 +13,17 @@ const int height = 800;
 
 
 int main(int argc, char** argv) {
-	imodel = new Model("obj/diablo3_pose.obj");
+	model = new Model("obj/diablo3_pose.obj");
 	TGAImage image(width, height, TGAImage::RGB);
 	for (int i=0; i<model->nfaces(); i++) {
 		std::vector<int> face = model->face(i);
-		for (int j=0; j<3; j++) {
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x+1.)*width/2.;
-			int y0 = (v0.y+1.)*height/2.;
-			Vec2f p1(x0,y0);
-			int x1 = (v1.x+1.)*width/2.;
-			int y1 = (v1.y+1.)*height/2.;
-			Vec2f p2(x1,y1);
-			line(p1,p2, image, white);
-		}
+		Vec2i screen_coords[3];
+    for (int j=0; j<3; j++) {
+        Vec3f world_coords = model->vert(face[j]);
+        screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.);
+    }
+    triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(rand()%255, rand()%255, rand()%255, 255));
+
 	}
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
